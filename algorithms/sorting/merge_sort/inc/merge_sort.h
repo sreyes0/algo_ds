4f69a089246a1b274478f3fb389f452bd549eda5
sreyes0 <sreyes.haeusler@gmail.com>
2026-03-21 00:55:04 +0100

    refactor(merge_sort): Prepare merge sort files

---
+++ algorithms/sorting/merge_sort/inc/merge_sort.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+#define DESC 0
+
+/**
+ * Sort a given vector of integers in-place using the merge sort 
+ * algorithm.
+ * 
+ * @param[in,out] array     Vector to be sorted
+ */
+void merge_sort(std::vector<int>& array);
---
+++ algorithms/sorting/merge_sort/src/merge_sort.cpp
@@ -0,0 +1,6 @@
+#include "../inc/merge_sort.h"
+
+
+void merge_sort(std::vector<int> &array) {
+
+}
