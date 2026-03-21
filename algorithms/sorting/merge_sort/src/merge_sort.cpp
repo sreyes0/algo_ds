e3e284a61506a4d3f8eef1babb1f76c720591746
sreyes0 <sreyes.haeusler@gmail.com>
2026-03-21 13:40:38 +0100

    feat(merge_sort): Add first implementation. Should optimize it to minimize copying data.

--- algorithms/sorting/merge_sort/src/merge_sort.cpp
+++ algorithms/sorting/merge_sort/src/merge_sort.cpp
@@ -1,22 +1,42 @@
 #include "../inc/merge_sort.h"
 
+#include <cassert>
 #include <limits>
 
 
 void merge_sort(std::vector<int> &array) 
 {
+    // End of recursion
+    if (array.size() <= 1) {
+        return;
+    }
 
+    // Split array
+    int mid_index = array.size() / 2;
+    std::vector<int>::iterator middle = array.begin() + mid_index;
+
+    std::vector<int> array1(array.begin(), middle);
+    std::vector<int> array2(middle, array.end());
+
+    assert(array.size() == array1.size() + array2.size());
+
+    // Recursive sort for each half
+    merge_sort(array1);
+    merge_sort(array2);
+
+    // Merge sorted results
+    array = merge(array1, array2);
 }
 
 std::vector<int> merge(std::vector<int> array1, std::vector<int> array2)
 {
     int total_size = array1.size() + array2.size();
     std::vector<int> array;
-    // Reserve allocates memory but does not instantiate anything
-    array.reserve(total_size);
+    array.reserve(total_size);  // Reserve allocates memory but does not
+                                // instantiate anything
 
-    // Append MAX_INT to end of both arrays so
-    // we don't have to check for limits in the loop
+    // Append MAX_INT to end of both arrays so we don't have to 
+    // check for array limits in the loop
     array1.push_back(std::numeric_limits<int>::max());
     array2.push_back(std::numeric_limits<int>::max());
 
